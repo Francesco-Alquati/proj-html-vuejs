cc750aa94e0f016203489a02f0a1349f7a09dccd
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-09-01 17:44:30 +0200

    [UPD] Added contacts  in header section

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 
+export default{
+  components:{
+    AppHeader,
+  }
+}
 </script>
 
 <template>
-  
+  <header>
+    <AppHeader />
+  </header>
+  <main>
+
+  </main>
+  <footer>
+
+  </footer>
 </template>
 
 <style lang="scss">
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <header>
+        <div class="container-fluid">
+            <div class="row contacts border-bottom border-black">
+                <div class="col-6">
+                    <div class="numbers">
+                        <span class="landline">(001)88451234</span>
+                        <span class="mobile ms-3">88455438</span>
+                    </div>
+                </div>
+                <div class="col-6 text-end">
+                    <i class="fa-brands fa-facebook-f"></i>
+                    <i class="fab fa-twitter"></i>
+                    <i class="fab fa-pinterest-p"></i>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+
+                </div>
+                <div class="col-6">
+
+                </div>
+                <div class="col-3">
+                    
+                </div>
+            </div>
+        </div>
+    </header>
+
+</template>
+<style lang="scss" scoped>
+.contacts{
+    height: 30px;
+    background-color: #3B3B3B;
+    color: white;
+    font-size: 12px;
+}
+.numbers{
+    padding: 5px;
+    margin-left: 30px;
+}
+i{
+    margin-right: 8px;
+    font-size: 15px;
+    padding: 6px;
+}
+
+</style>
